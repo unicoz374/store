1a0454b5cc7c9108602e387434a046175dba29fb
unicoz374 <banserricardo670@gmail.com>
2026-09-10 16:20:34 +0700

    Delete toko-app directory

--- toko-app/src/app/admin-22c484081cc4/page.tsx
+++
@@ -1,8 +0,0 @@
-import { isAdminLoggedIn } from "@/lib/admin-auth";
-import AdminLoginForm from "@/components/admin/AdminLoginForm";
-import AdminDashboard from "@/components/admin/AdminDashboard";
-
-export default function AdminPage() {
-  const loggedIn = isAdminLoggedIn();
-  return loggedIn ? <AdminDashboard /> : <AdminLoginForm />;
-}
